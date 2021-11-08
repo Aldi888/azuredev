c11d8906e8a6a55bb6c666cb69860d786a85b499
aldi <40169111+Aldi888@users.noreply.github.com>
2021-11-07 21:28:40 -0300

    app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning and sofering with React
         </a>
       </header>
     </div>
