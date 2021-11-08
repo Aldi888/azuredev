2515acbba731222b30c27fa9b8e77460a4ecc0c1
aldi <40169111+Aldi888@users.noreply.github.com>
2021-11-08 10:33:29 -0300

    sync

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning and sofering with React
+          Learning and sofering with React testing new sync
         </a>
       </header>
     </div>
